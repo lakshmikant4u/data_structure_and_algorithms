a4c75aaf6c698f40b72e5fc9493a9de40847fef7
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-09 19:36:53 +0530

    Solution 2 with sorting

--- techiedelight/easy/array/basic/find-pair-with-given-sum-array.js
+++ techiedelight/easy/array/basic/find-pair-with-given-sum-array.js
@@ -43,3 +43,29 @@ const pairWithGivenSum = (nums, target) => {
 
 pairWithGivenSum([8, 7, 2, 5, 3, 1], 10); // Pair found (8, 2);
 
+
+// Solution 2 Using Sorting
+
+const pairWithGivenSum2 = (nums, target) => {
+    nums.sort((a, b) => a - b);
+    let low = 0;
+    let high = nums.length - 1;
+    while (low < high) {
+
+        if (nums[low] + nums[high] == target) {
+            console.log(`Pair found (${nums[low]}, ${nums[high]})s`);
+            return;
+        }
+        if (nums[low] + nums[high] < target) {
+            low++;
+        }
+        else {
+            high--;
+        }
+    }
+
+    console.log("Pair not found");
+}
+
+pairWithGivenSum2([8, 7, 2, 5, 3, 1], 8); // Pair found (1, 7);
+
