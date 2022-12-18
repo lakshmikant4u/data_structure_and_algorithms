3ef3ae64a77beea7e9738f59311fe0dcf894ed20
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-18 13:16:37 +0530

    Check if two numbers have opposite signs using bit manipulation

--- techiedelight/easy/bitmap/bit-hacks-part-1-basic.js
+++ techiedelight/easy/bitmap/bit-hacks-part-1-basic.js
@@ -11,4 +11,23 @@ const checkOddOrEven = num => {
 }
 
 checkOddOrEven(8);
+console.log("~~~~~~~~~~~~~~~")
 checkOddOrEven(9);
+console.log("~~~~~~~~~~~~~~~")
+
+const checkOppositeSigns = (a, b) => {
+    console.log(`${a} in binary is ${a.toString(2)}`)
+    console.log(`${b} in binary is ${b.toString(2)}`)
+
+    let isOpposite = ((a ^ b) < 0);
+    if (isOpposite) {
+        console.log(`${a} & ${b} have opposite signs`);
+    } else {
+        console.log(`${a} & ${b} have same signs`);
+    }
+}
+
+checkOppositeSigns(3, -4);
+console.log("~~~~~~~~~~~~~~~")
+checkOppositeSigns(6, 14);
+console.log("~~~~~~~~~~~~~~~")
