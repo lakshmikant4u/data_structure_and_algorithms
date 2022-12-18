e44506cea828e6066e095e045602b4ea7b10023a
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-18 11:35:25 +0530

    Bit Manipulation Basic Odd or Even

---
+++ techiedelight/easy/bitmap/bit-hacks-part-1-basic.js
@@ -0,0 +1,14 @@
+// https://www.techiedelight.com/bit-hacks-part-1-basic/
+
+// Problem 1. Check if an integer is even or odd
+
+const checkOddOrEven = num => {
+    if ((num & 1) != 0) {
+        console.log(`${num} is even`);
+    } else {
+        console.log(`${num} is even`);
+    }
+}
+
+checkOddOrEven(8);
+checkOddOrEven(9);
