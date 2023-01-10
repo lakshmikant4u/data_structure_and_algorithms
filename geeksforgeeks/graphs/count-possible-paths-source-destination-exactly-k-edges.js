79563a8f0fc414ab350b747f00d6e8f63f80fdcb
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-01-11 00:20:47 +0530

    Solution 2 for counting path

--- geeksforgeeks/graphs/count-possible-paths-source-destination-exactly-k-edges.js
+++ geeksforgeeks/graphs/count-possible-paths-source-destination-exactly-k-edges.js
@@ -45,3 +45,62 @@ var u = 0, v = 3, k = 2;
 console.log(countwalks1(graph, u, v, k));
 
 
+
+// Number of vertices in the graph
+var V = 4
+
+// A Dynamic programming based function to count walks from u
+// to v with k edges
+function countwalks2(graph, u, v, k) {
+    // Table to be filled up using DP.
+    // The value count[i][j][e] will
+    // store count of possible walks from
+    // i to j with exactly k edges
+    var count = new Array(V);
+    for (var i = 0; i < V; i++) {
+        count[i] = new Array(V);
+        for (var j = 0; j < V; j++) {
+            count[i][j] = new Array(V);
+            for (var e = 0; e <= k; e++) {
+                count[i][j][e] = 0;
+            }
+        }
+    }
+
+    // Loop for number of edges from 0 to k
+    for (var e = 0; e <= k; e++) {
+        for (var i = 0; i < V; i++) // for source
+        {
+            for (var j = 0; j < V; j++) // for destination
+            {
+                // initialize value
+                count[i][j][e] = 0;
+
+                // from base cases
+                if (e == 0 && i == j)
+                    count[i][j][e] = 1;
+                if (e == 1 && graph[i][j])
+                    count[i][j][e] = 1;
+
+                // go to adjacent only when the
+                // number of edges is more than 1
+                if (e > 1) {
+                    for (var a = 0; a < V; a++) // adjacent of source i
+                        if (graph[i][a])
+                            count[i][j][e] += count[a][j][e - 1];
+                }
+            }
+        }
+    }
+    return count[u][v][k];
+}
+
+// driver program to test above function
+/* Let us create the graph shown in above diagram*/
+var graph = [[0, 1, 1, 1],
+[0, 0, 0, 1],
+[0, 0, 0, 1],
+[0, 0, 0, 0]];
+var u = 0, v = 3, k = 2;
+console.log(countwalks2(graph, u, v, k));
+
