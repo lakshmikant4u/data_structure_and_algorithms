0cfc5e1a974186a068875951efb492426bd5c707
lakshmikant4u <lakshmikant4u@gmail.com>
2023-03-30 09:40:17 +0530

    Recursive Binary Search

--- leetcode/medium/array/peak-index-in-a-mountain-array.js
+++ leetcode/medium/array/peak-index-in-a-mountain-array.js
@@ -54,3 +54,20 @@ const peakIndexInMountainArray2 = (arr, pos = 0) => {
 };
 console.log(peakIndexInMountainArray2([0, 2, 3, 4, 1, 0])); // 3
 
+// Solution 3 : Recursive Binary Search
+const peakIndexInMountainArray3 = (arr, low = 0, high = arr.length - 1) => {
+    if (!arr.length)
+        return -1
+
+    const mid = ((low + high) / 2) | 0
+
+    if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+        return mid
+
+    if (arr[mid] < arr[mid - 1])
+        return peakIndexInMountainArray(arr, low, mid)
+
+    if (arr[mid] < arr[mid + 1])
+        return peakIndexInMountainArray(arr, mid, high)
+}
+console.log(peakIndexInMountainArray3([0, 10, 15, 5, 2])); // 2
